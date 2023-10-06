2e28e694e741673166b00110a70a6a2f86bdbfdd
nothke <nothke@gmail.com>
2023-10-06 16:27:18 +0200

    Added Joint detach, rearranged

--- Utils.cs
+++ Utils.cs
@@ -5,6 +5,25 @@ namespace Nothke
 {
 	public static class Utils
 	{
+		// Nodes
+
+		/// <summary>
+		/// Creates a Node of type T and parents to parent or if null it attaches it to the scene root
+		/// </summary>
+		public static T Instantiate<T>(this Node node, Node parent = null) where T : Node, new()
+		{
+			var newNode = new T();
+
+			if (parent == null)
+				node.GetTree().Root.AddChild(newNode);
+			else
+				parent.AddChild(newNode);
+
+			return newNode;
+		}
+
+		// 3D
+
 		public static Vector3 Forward(this Node3D node3D)
 		{
 			return -node3D.GlobalTransform.Basis.Z;
@@ -20,6 +39,8 @@ namespace Nothke
 			return node3D.GlobalTransform.Basis.X;
 		}
 
+		// Input
+
 		/// <summary>
 		/// Returns -1 when negative is pressed and 1 when positive is pressed, 0 when nothing
 		/// </summary>
@@ -28,19 +49,12 @@ namespace Nothke
 			return Input.IsKeyPressed(positive) ? 1 : (Input.IsKeyPressed(negative) ? -1 : 0);
 		}
 
-		/// <summary>
-		/// Creates a Node of type T and parents to parent or if null it attaches it to the scene root
-		/// </summary>
-		public static T Instantiate<T>(this Node node, Node parent = null) where T : Node, new()
+		// Physics
+
+		public static void Detach(this Joint3D joint)
 		{
-			var newNode = new T();
-
-			if (parent == null)
-				node.GetTree().Root.AddChild(newNode);
-			else
-				parent.AddChild(newNode);
-
-			return newNode;
+			joint.NodeA = null;
+			joint.NodeB = null;
 		}
 	}
 }
